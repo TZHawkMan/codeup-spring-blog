d413e7f87866ab32d3b22ac60c4b24f073ef335b
Tianrun Zhang <zhang_tianrun@yahoo.com>
2023-02-16 16:11:59 -0600

    controller exercise

---
+++ src/main/java/com/codeup/codeupspringblog/MathController.java
@@ -0,0 +1,37 @@
+package com.codeup.codeupspringblog;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+    @GetMapping("/add/3/and/4")
+    @ResponseBody
+
+    public int adding() {
+        return 3 + 4;
+    }
+
+    @GetMapping("/subtract/3/from/10")
+    @ResponseBody
+
+    public int subtracting() {
+        return 10 - 7;
+    }
+
+    @GetMapping("/multiply/4/and/5")
+    @ResponseBody
+
+    public int multipling() {
+        return 4 * 5;
+    }
+
+    @GetMapping("/divide/6/by/3")
+    @ResponseBody
+
+    public int deviding() {
+        return 6/3;
+    }
+}
